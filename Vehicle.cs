ccbc590b6ab6a00333c510af5a6a154e6ef551a4
Billybob <danktempbillybob@gmail.com>
2022-05-02 19:40:11 -0700

    Vehicle class created/added

---
+++ Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WC.SARS
+{
+    public enum VehicleType
+    {
+        Hamsterball,
+        Emu
+    }
+    internal class Vehicle
+    {
+        public byte HP;
+        public short VehicleID;
+        public Vehicle(byte hp, short vehicleid)
+        {
+            HP = hp;
+            VehicleID = vehicleid;
+        }
+    }
+}
